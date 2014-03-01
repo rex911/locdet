d504f0967f88510a34d0f34f1ec53cda89ca6f7c
rex911 <rex@Rex-MacBook-Pro.local>
2014-02-28 19:32:22 -0500

    Firsr commit

---
+++ src/locdet/Test.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package locdet;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import edu.cmu.minorthird.text.*;
+import edu.cmu.minorthird.text.learn.*;
+import edu.cmu.minorthird.text.learn.SequenceAnnotatorLearner.SequenceAnnotator;
+import edu.cmu.minorthird.text.learn.experiments.*;
+import edu.cmu.minorthird.classify.Splitter;
+import edu.cmu.minorthird.classify.experiments.CrossValSplitter;
+import edu.cmu.minorthird.classify.experiments.RandomSplitter;
+import edu.cmu.minorthird.classify.sequential.*;
+import edu.cmu.minorthird.ui.*;
+
+/**
+ * @author rex
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws NumberFormatException, IOException, ParseException {
+		// TODO Auto-generated method stub
+		String textDir = "train/";	
+		String labelDir = "train.labels";
+		Corpus corpus = new Corpus(textDir, labelDir, new TwitterTokenizer());
+		String label = "Location";
+		TextLabelsAnnotatorTeacher teacher = new TextLabelsAnnotatorTeacher(corpus.getTextLabels(),
+				"city");
+				
+		SequenceAnnotatorLearner learner = new SequenceAnnotatorLearner(new CRFLearner(), new Recommended.TokenFE());
+		//SequenceAnnotatorLearner.SequenceAnnotator ann = (SequenceAnnotator) teacher.train(learner);
+		RandomSplitter s = new RandomSplitter(0.7);
+		CrossValSplitter cvs = new CrossValSplitter(5);
+		cvs.split(corpus.getTextLabels().instanceIterator(label));
+		s.split(corpus.getTextLabels().instanceIterator(label));
+		TextLabelsExperiment expt = new TextLabelsExperiment(
+				corpus.getTextLabels(), 
+				s,
+				learner,
+				label, label + "_prediction");
+		expt.doExperiment();
+		
+	}
+
+}
